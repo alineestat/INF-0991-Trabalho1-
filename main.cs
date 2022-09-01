d864b7e5a36b04573f219d7917588872a963ffdd
Aline Dias <>
2022-09-01 19:40:25 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,29 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+
+  sbyte s = 20;
+  short t = -32;
+  int i = -1;
+  long l = -3000;
+  Console.WriteLine($"sbyte s= {s}"); 
+
+    // tipos de Dados Booleanos
+
+    bool a = true;
+    bool b = false;
+
+    int x = 1;
+    int y = 2;
+    int z = 1;
+
+    Console.WriteLine($" x = y {x == y} pois, x={x} e y={y}"); 
+    
+    Console.WriteLine($"x < y {x < y} pois, x={x} e y={y}"); 
+
+
+
+  }
+}
